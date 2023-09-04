57f1099c6eda8ecc9f7813b5d18a2550ff20e780
Gabriel A. López López <glpz@daxslab.com>
2023-09-04 00:08:28 -0400

    Add list of post in blog page

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
 
+const contentQuery = await queryContent('blog')
+    .sort({ date: -1 })
+    .find()
+
 </script>
 
 <template>
-  this is the content of the blog directory
+  <h1>My posts</h1>
+  <p></p>
+
+  <ul>
+    <li v-for="post in contentQuery" :key="post._path">
+      <h2><NuxtLink :to="`${post._path}`">{{ post.title }}</NuxtLink></h2>
+      <p>{{ post.date }} - {{ post.description }}</p>
+    </li>
+  </ul>
 </template>
-
-<style scoped>
-
-</style>
