18b0212a0e31489711fde23814182aeba5096633
Gabriel A. López López <glpz@daxslab.com>
2024-01-18 00:19:56 -0500

    Fix for other pages

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,13 @@
 <template>
-  <main>
-    <ContentDoc />
-  </main>
+    <ContentDoc v-slot="{ doc }">
+      <article>
+        <header>
+          <h1>{{ doc.title }}</h1>
+          <p v-if="doc._dir=='blog'" class="description">
+            <time>{{doc.date}}</time> - {{ doc.description }}
+          </p>
+        </header>
+        <ContentRenderer :tag="false" :value="doc" />
+      </article>
+    </ContentDoc>
 </template>
--- pages/blog.vue
+++ pages/blog.vue
@@ -1,9 +1,38 @@
 <script setup lang="ts">
 
+useHead({
+  title: "Blog"
+})
+
+const query = {
+  sort: {
+    date: -1
+  }
+}
+
 </script>
 
 <template>
-  this is the content of the blog directory
+
+  <h1>Blog</h1>
+
+  <ContentList
+      path="/blog"
+      :query="query"
+      v-slot="{ list }">
+    <ul>
+      <li v-for="article in list" :key="article._path">
+        <article>
+          <h2>
+            <NuxtLink :to="article._path">
+              {{ article.title }}
+            </NuxtLink>
+          </h2>
+          <p>{{ article.description }}</p>
+        </article>
+      </li>
+    </ul>
+  </ContentList>
 </template>
 
 <style scoped>
